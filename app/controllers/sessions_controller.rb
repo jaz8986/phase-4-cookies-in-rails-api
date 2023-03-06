b129f91fc32ddf5d65aee4bfe37ad29d9c4323d0
Jaskiran Singh <jsingh8986@yahoo.com>
2023-03-05 21:11:26 -0800

    commit tree 119e1c83f5bec82ef192319e7dcfa02dab2d4116

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,10 @@
 class SessionsController < ApplicationController
   
   def index
+    byebug
     session[:session_hello] ||= "World"
     cookies[:cookies_hello] ||= "World"
+    cookies[:cookies_dope] ||= "weed"
     render json: { session: session, cookies: cookies.to_hash }
   end
 
